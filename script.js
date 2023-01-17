c9382c3036baed7098287d7a070b8dc5771f3414
groot96 <104350826+groot96@users.noreply.github.com>
2023-01-17 20:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-	const len = Str.length;
-for(let i=0; i<len/2; i++){
-    if (str[i] !== str[len-1-i]) {
-        return 'false';
-    }
-}
-return 'true';
+	const reversed = str.split('').reverse().join('');
+  return str === reversed;
 
 }
 module.exports = palindrome
